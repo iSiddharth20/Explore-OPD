fe8d0b2c95f0c9626f253401a5856ee7f9388007
Siddharth Kekre <kekresiddharth20@gmail.com>
2024-07-04 01:53:54 -0700

    Main: Implemented UI Buttons to Import and PreProcess Document

--- Code/main.py
+++ Code/main.py
@@ -5,25 +5,40 @@ Talks to GUI, PreProcess, LLM, DB, Server
 
 # Import Necessary Modules
 from preprocess import PreProcess
+from gui import GUI
+
+# Import Necessary Libraries
+from tkinter import filedialog
 
 
 class ExploreOPD:
     def __init__(self):
         self.pdf_path = None
         self.preprocess = PreProcess()
+        self.knowledge_base = self.preprocess.knowledge_base
+        self.gui = GUI()
 
-    def get_path(self, path):
+    def set_path(self, path=None):
         """ Updates the Path of PDF File """
-        self.pdf_path = path
+        if path:
+            self.pdf_path = path
+        else:
+            self.pdf_path = filedialog.askopenfilename()
 
     def update_knowledge_base(self):
         """ Updates Knowledge Base with Extracted Data """
         self.preprocess.extract_from_pdf(self.pdf_path)
 
+    def app(self):
+        button_get_file = self.gui.action_button("Select Source File", command=self.set_path)
+        button_get_file.pack()
+
+        button_proceed = self.gui.action_button("Update Knowledge_Base", command=self.update_knowledge_base)
+        button_proceed.pack()
+
 
 if __name__ == "__main__":
     testrun = ExploreOPD()
-    pdf_path = r"../SampleDocuments/SampleDoc.pdf"
-    testrun.get_path(pdf_path)
-    testrun.update_knowledge_base()
-    print(f"""For the File Path: "{pdf_path}",\nThe Knowledge Base is:\n{testrun.preprocess.knowledge_base}""")
+    testrun.app()
+    testrun.gui.start()
+    print(f"""For the File Path: "{testrun.pdf_path}",\nThe Knowledge Base is:\n{testrun.knowledge_base}""")
