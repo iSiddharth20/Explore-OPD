64c4cff6e5106e6011368645ac53e9ac682376e0
iSiddharth20 <kekresiddharth20@gmail.com>
2024-09-16 01:08:05 +0000

    downloadfile: Can Download Multiple PDF Files

---
+++ src/downloadfile.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import json
+
+class PDFDownloader:
+    def __init__(self, url_dict):
+        self.url_dict = url_dict
+
+    def download_pdfs(self):
+        for filename, url in self.url_dict.items():
+            pdf_path = os.path.join("../DocumentCorpus", filename)
+            if not os.path.exists(pdf_path):
+                response = requests.get(url)
+                if response.status_code == 200:
+                    with open(pdf_path, "wb") as file:
+                        file.write(response.content)
+                    print(f"The file has been downloaded and saved as {pdf_path}")
+                else:
+                    print(f"Failed to download the file from {url}. Status code: {response.status_code}")
+            else:
+                print(f"File {pdf_path} exists.")
+
+if __name__ == "__main__":
+    json_input = '''
+    {
+        "fielding_dissertation.pdf": "https://ics.uci.edu/~fielding/pubs/dissertation/fielding_dissertation.pdf",
+        "human_nutrition_text.pdf": "https://pressbooks.oer.hawaii.edu/humannutrition2/open/download?type=pdf"
+    }
+    '''
+    url_dict = json.loads(json_input)
+    downloader = PDFDownloader(url_dict)
+    downloader.download_pdfs()
