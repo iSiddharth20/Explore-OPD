83e9e0b9521c87630bd01963048074ed178e6925
iSiddharth20 <kekresiddharth20@gmail.com>
2024-09-17 23:29:58 +0000

    Sentence Extraction using spaCy

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,11 +1,11 @@
 '''
-Pre-Processing the Documents and Create Chunks
-Talks to Main, GUI, LLM
+Extracts Data (Text Paragraphs) and Metadata (FileName, PageNumber) from PDF Documents
 '''
 
 # Import Necessary Libraries
 import os
 import pymupdf as fitz
+from spacy.lang.en import English
 
 
 class PreProcess:
@@ -13,24 +13,24 @@ class PreProcess:
         self.knowledge_base = []
 
     def append_to_knowledge_base(self, filename, page_number, text=None):
-        """ Add Extracted & Formatted Data to Knowledge Base """
+        """ Add Extracted & Formatted Data to Knowledge Base of this Document"""
         self.knowledge_base.append({"filename": filename,
                                     "page_number": page_number+1,
                                     "text": text})
 
     def format_text(self, text):
-        """ Format the Extracted Text """
+        """ Split the Extracted Text into Sentences using spaCy """
+        nlp = English()
+        nlp.add_pipe("sentencizer")
         text = text.replace('\n', ' ').strip()
-        sentences = text.split('. ')
-        sentences = [sentence.strip() for sentence in sentences]
+        sentences = list(nlp(text).sents)
+        sentences = [str(sntc) for sntc in sentences]
         return sentences
 
-    def extract_from_pdf(self, doc_path):
+    def extract_from_pdf(self, filename, doc_path):
         """ Extract Contents from PDF and Store it with Metadata (Filename, Page Number, Content) """
         doc = fitz.open(doc_path, filetype="pdf")
-        filename = os.path.basename(doc_path)
         for page_number, page in enumerate(doc):
-            # Extract Text from the Page
             text = page.get_text()
             text = self.format_text(text)
             self.append_to_knowledge_base(filename, page_number, text=text)
@@ -39,5 +39,6 @@ class PreProcess:
 if __name__ == "__main__":
     testrun = PreProcess()
     pdf_path = r"../DocumentCorpus/fielding_dissertation.pdf"
-    testrun.extract_from_pdf(pdf_path)
-    print(f"""For the File Path: "{pdf_path}",\nSlice of Knowledge Base is:\n{testrun.knowledge_base[0]}""")
+    filename = "fielding_dissertation"
+    testrun.extract_from_pdf(filename, pdf_path)
+    print(f"""For the File Path: "{pdf_path}",\nSlice of Knowledge Base is:\n{testrun.knowledge_base[10]}""")
